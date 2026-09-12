f7809b9dd86e01eb362c23ff3c4a63a3a53cb932
Zudo45 <alex.020fr@gmail.com>
2026-09-12 18:44:40 -0500

    actualizar

--- contracts/1_Storage.sol
+++ contracts/1_Storage.sol
@@ -2,15 +2,19 @@
 
 pragma solidity >=0.8.2 <0.9.0;
 
+/*
+
+	Primer contrato
+	ASBV
+
+*/
 contract Storage {
 
-    uint256 number = 5;
+    uint256 number; //uint256 = uint
 
- 
     function store(uint256 num) public {
         number = num;
     }
-
 
     function retrieve() public view returns (uint256){
         return number;
